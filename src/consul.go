d2a77aca47f9ab7eaae93d3add6de2bbdfa6dc71
Stuart Dallas <stuart@stut.net>
2023-10-08 10:26:04 +0100

    Cleaned up logging in domain discovery

--- src/consul.go
+++ src/consul.go
@@ -34,9 +34,8 @@ func (manager *Manager) discoverDomainsFromConsul() ([]string, error) {
 					if strings.Contains(url, ":") {
 						separator = ":"
 					}
-					log.Infof("aleff: Found tag: %s", url)
 					stripped := strings.SplitN(url, separator, 2)[0]
-					log.Infof("aleff: Stripped domain: %s", url)
+					log.Infof("aleff: Found tag \"%s\", strips to \"%s\"", url, stripped)
 					domains = append(domains, stripped)
 				}
 			} else if tag == "aleff-enabled" {
